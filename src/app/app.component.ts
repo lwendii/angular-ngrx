6e4c1368fb41fe023ab5f63bf84e4d126fbc6e7f
Li <wendyli8620lw@gmail.com>
2023-02-21 23:03:06 +0100

    added ngrx example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { BooksService } from './book-list/books.service';
+import { BooksApiActions, BooksActions } from './state/books.action';
+import { selectBooks, selectBookCollection } from './state/books.selectors';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-ngrx';
+
+  books$ = this.store.select(selectBooks);
+  bookCollection$ = this.store.select(selectBookCollection);
+
+  constructor(private booksService: BooksService, private store: Store) {}
+
+  ngOnInit(): void {
+    this.booksService
+      .getBooks()
+      .subscribe((books) =>
+        this.store.dispatch(BooksApiActions.retrievedBookList({ books }))
+      );
+  }
+
+  onAdd(bookId: string) {
+    this.store.dispatch(BooksActions.addBook({ bookId }));
+  }
+
+  onRemove(bookId: string) {
+    this.store.dispatch(BooksActions.removeBook({ bookId }));
+  }
 }
